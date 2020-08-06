14747c48f1f944ceb83621a545f622f5b956692f
zyl-pc <787193801@qq.com>
2020-08-06 18:29:13 +0800

    go语法

--- controllers/Home.go
+++ controllers/Home.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"github.com/astaxie/beego"
+	"unsafe"
 )
 
 type Home struct {
@@ -10,7 +11,17 @@ type Home struct {
 }
 
 func (c *Home) Index() {
+	//基本操作()
+	//数据类型()
+	//变量()
+	//常量()
+	//运算符()
+	//条件语句()
+	//循环语句()
+	函数()
+}
 
+func 基本操作() {
 	//int [指定类型]
 	var i int = 100
 
@@ -26,6 +37,266 @@ func (c *Home) Index() {
 	fmt.Printf("float:%.2f\n", f)
 }
 
-func (c *Home) Get() {
+func 数据类型() {
+
+	var b bool = true
+	var i int = 0xFFFFFFFFFFFFFFFF / 2     //(int)根据当前系统32位 或者64位 8字节 -9223372036854775808 ~ 9223372036854775807 (18446744073709551615)
+	var i8 int8 = 0xFF / 2                 //-128 ~ 127  (255)
+	var i16 int16 = 0xFFFF / 2             //-32768 ~ 32767 (65535)
+	var i32 int32 = 0xFFFFFFFF / 2         //-2147483648 ~ 2147483647 (4294967295)
+	var i64 int64 = 0xFFFFFFFFFFFFFFFF / 2 //-9223372036854775808 ~ 9223372036854775807 (18446744073709551615)
+	var ui uint = 1
+	var ui8 uint8 = 1
+	var ui16 uint16 = 1
+	var ui32 uint32 = 1
+	var ui64 uint64 = 1
+	var f32 float32 = 0.5
+	var f64 float64 = 0.55
+	var str string = "string"
+	var bt byte = 0b000000101
+	var re rune = 0xFFFFFFFF / 2
+	var ip *uint = &ui
+
+	//fmt.Println(b,i,i8,i16,i32,i64,ui,ui8,ui16,ui32,ui64,f32,f64,str);
+	fmt.Printf("b\t=> %v \t%d byte\n", b, unsafe.Sizeof(b))
+	fmt.Printf("i\t=> %d \t%d byte\n", i, unsafe.Sizeof(i))
+	fmt.Printf("i8\t=> %d \t%d byte\n", i8, unsafe.Sizeof(i8))
+	fmt.Printf("i16\t=> %d \t%d byte\n", i16, unsafe.Sizeof(i16))
+	fmt.Printf("i32\t=> %d \t%d byte\n", i32, unsafe.Sizeof(i32))
+	fmt.Printf("i64\t=> %d \t%d byte\n", i64, unsafe.Sizeof(i64))
+	fmt.Printf("ui\t=> %d \t%d byte\n", ui, unsafe.Sizeof(ui))
+	fmt.Printf("ui8\t=> %d \t%d byte\n", ui8, unsafe.Sizeof(ui8))
+	fmt.Printf("ui16\t=> %d \t%d byte\n", ui16, unsafe.Sizeof(ui16))
+	fmt.Printf("ui32\t=> %d \t%d byte\n", ui32, unsafe.Sizeof(ui32))
+	fmt.Printf("ui64\t=> %d \t%d byte\n", ui64, unsafe.Sizeof(ui64))
+	fmt.Printf("f32\t=> %f \t%d byte\n", f32, unsafe.Sizeof(f32))
+	fmt.Printf("f64\t=> %f \t%d byte\n", f64, unsafe.Sizeof(f64))
+	fmt.Printf("str\t=> %s \t%d byte\n", str, unsafe.Sizeof(str))
+	fmt.Printf("bt\t=> %d \t%d byte\n", bt, unsafe.Sizeof(bt))
+	fmt.Printf("re\t=> %d \t%d byte\n", re, unsafe.Sizeof(re))
+	fmt.Printf("ip\t=> %d \t%d byte\n", ip, unsafe.Sizeof(ip))
+
+}
+
+func 变量() {
+	//声明单个
+	var i int = 100
+	fmt.Printf("i\t=> %d\n", i)
+
+	//声明多个
+	var i1, i2 int = 1000, 2000
+	fmt.Printf("i1\t=> %d\n", i1)
+	fmt.Printf("i2\t=> %d\n", i2)
+
+	//根据值自行判定变量类型
+	var str = "string"
+	fmt.Printf("str\t=> %s\n", str)
+
+	//省略 var
+	vBool := true
+	fmt.Printf("vBool\t=> %v\n", vBool)
+
+	//定义多个不同类型的变量
+	var (
+		i3   int    = 1
+		str1 string = "string1"
+	)
+	fmt.Printf("i3\t=> %v\n", i3)
+	fmt.Printf("str1\t=> %v\n", str1)
+
+	//不同类型值可多赋值
+	var i4 int
+	var str2 string
+	i4, str2 = 100, "string2"
+	fmt.Printf("i4\t=> %v\n", i4)
+	fmt.Printf("str2\t=> %v\n", str2)
+
+	//不同类型进行初始化声明
+	i5, str3, f := 100, "string3", 0.5
+	fmt.Printf("i5\t=> %v\n", i5)
+	fmt.Printf("str3\t=> %v\n", str3)
+	fmt.Printf("f\t=> %v\n", f)
+
+	//相同类型相互交换值
+	var i6, i7 int = 1, 2
+	i6, i7 = i7, i6
+	fmt.Printf("i6\t=> %v\n", i6)
+	fmt.Printf("i7\t=> %v\n", i7)
+}
+
+func 常量() {
+
+	//单个声明初始化
+	const i int = 100
+	//省略类型
+	const str = "string"
+	//多变量声明
+	const i1, str1 = 100, "string1"
+
+	//枚举 iota每次递增
+	const (
+		stand int = iota
+		walk
+		run
+	)
+
+	fmt.Printf("stand: %d\n", stand)
+	fmt.Printf("walk: %d\n", walk)
+	fmt.Printf("run: %d\n", run)
+}
+
+func 运算符() {
+
+	//加法 +
+	var num = 5.0
+	var money = 12.5
+
+	num = num + 4
+	fmt.Printf("num\t=> %.2f\n", num)
+
+	//减法 -
+	num = num - 2
+	fmt.Printf("num\t=> %.2f\n", num)
+
+	//乘法 *
+	var total = money * num
+	fmt.Printf("total\t=> %.2f\n", total)
+
+	//除法 /
+	var avg = total / num
+	fmt.Printf("avg\t=> %.2f\n", avg)
+
+	/*
+		num     => 9.00
+		num     => 7.00
+		total   => 87.50
+		avg     => 12.50
+	*/
+
+	//运算符
+	var a = 1
+	var b = 1
+	var c = 2
+	if a == b {
+		fmt.Println("a == b")
+	}
+	if c != b {
+		fmt.Println("c != b")
+	}
+	if c != b {
+		fmt.Println("c != b")
+	}
+	if c >= b {
+		fmt.Println("c >= b")
+	}
+	if b <= c {
+		fmt.Println("b <= c")
+	}
+	if a == b && c != b {
+		fmt.Println("a == b && c != b")
+	}
+
+	//位运算
+	var b1 byte = 0b1010
+	var b2 byte = 0b1100
+	fmt.Printf("b1 & b2\t=> %.8b\n", (b1 & b2))
+	fmt.Printf("b1 | b2\t=> %.8b\n", (b1 | b2))
+	fmt.Printf("b1 ^ b2\t=> %.8b\n", (b1 ^ b2))
+
+	//左右右移
+	var bb1 byte = 0b10001101
+	fmt.Printf("bb1>>4\t=> %.8b\n", (bb1 >> 4))
+	fmt.Printf("bb1<<4\t=> %.8b\n", (bb1 << 4))
+
+	//取地址 解引用
+	var i int = 4
+	var ptr *int = &i
+	fmt.Printf("i\t=> %d\n", i)
+	fmt.Printf("&i\t=> %x\n", &i)
+	fmt.Printf("ptr\t=> %x\n", ptr)
+	fmt.Printf("*ptr\t=> %d\n", *ptr)
+
+}
+
+func 条件语句() {
+
+	var b bool = true
+	var b1 bool = false
+
+	//if
+	if b {
+		fmt.Println("b = true")
+	} else if b1 {
+		fmt.Println("b1 = true")
+	} else {
+		fmt.Println("b 和 b1 都是 false")
+	}
+
+	//switch
+	var s1 = 3
+	switch s1 {
+	case 1:
+		fmt.Println("s1 = 1")
+		break
+	case 2:
+		fmt.Println("s1 = 2")
+		break
+	default:
+		fmt.Println("s1 = " + fmt.Sprintf("%d", s1))
+	}
+
+	//select
+	var c1, c2 chan int
+	var i1, i2 int
+	select {
+	case c1 <- i1:
+		fmt.Printf("received ", i1, " from c1\n")
+	case c2 <- i2:
+		fmt.Printf("sent ", i2, " to c2\n")
+	default:
+		fmt.Printf("no communication\n")
+	}
+}
+
+func 循环语句() {
+
+	//for循环
+	//(1)
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i)
+	}
+
+	//(2)
+	var i1 = 0
+	for ; i1 <= 5; i1++ {
+		fmt.Println(i1)
+	}
+
+	//(3)
+	sum := 5
+	for sum >= 0 {
+		fmt.Println(sum)
+		sum--
+	}
+
+	//(4) 无限循环
+	//for {
+	//fmt.Println(sum);
+	//sum--
+	//}
+
+	//(5)
+	str := []string{"google", "baidu"}
+	for index, value := range str {
+		fmt.Println(index, value)
+	}
+
+	number := [6]int{1, 2, 3, 4, 5}
+	for k, v := range number {
+		fmt.Println(k, v)
+	}
+}
+
+func 函数() {
 
 }
